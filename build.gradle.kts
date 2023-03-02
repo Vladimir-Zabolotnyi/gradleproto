56b874145f5d068fcbde1eabca25bcb9bebb84da
Volodymyr Zabolotniy <volodymyr.zabolotniy@leovegas.com>
2023-03-02 20:38:13 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.0.3"
+	id("io.spring.dependency-management") version "1.1.0"
+	id("com.google.protobuf") version "0.9.2"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("io.grpc:grpc-stub:1.15.1")
+	implementation("io.grpc:grpc-protobuf:1.15.1")
+	implementation("com.google.protobuf:protobuf-java:3.6.1")
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc:3.6.1"
+	}
+}
